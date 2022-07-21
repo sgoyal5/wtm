60be4cdda3722d2b81adb08c0952025591d6fce7
Sana Goyal <sgoyal@unc.edu>
2022-07-21 09:12:51 -0400

    Initial Commit

---
+++ SANA WTM FRONTEND/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SANA WTM FRONTEND
+//
+//  Created by Sana Goyal on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SANA WTM FRONTEND/SANA_WTM_FRONTENDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SANA_WTM_FRONTENDApp.swift
+//  SANA WTM FRONTEND
+//
+//  Created by Sana Goyal on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SANA_WTM_FRONTENDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
