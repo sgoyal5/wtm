138313dff9bfbedfdbc5a33202b26b7b88348c1f
Tanvi Poondota <tanvi.poondota@duke.edu>
2022-07-22 10:44:27 -0400

    added app delegate

--- SANA WTM FRONTEND/SANA_WTM_FRONTENDApp.swift
+++ SANA WTM FRONTEND/SANA_WTM_FRONTENDApp.swift
@@ -6,6 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      UITableView.appearance().backgroundColor = UIColor(red: 0.08235, green: 0.12549, blue: 0.12941, alpha: 100)
+      UIListContentView.appearance().backgroundColor = UIColor(red: 0.08235, green: 0.12549, blue: 0.12941, alpha: 100)
+      
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct wtm_App: App {
